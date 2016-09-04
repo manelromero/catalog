02b0271f41f54de2dbd6873ecb63c7fa4ca0a9f7
Manel Romero <manel@manelromero.com>
2016-09-04 21:54:28 +0200

    Change database

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 DEBUG = True
 GOOGLE_CLIENT_ID = '639902805989-ljom2kulh5pjt6894c9t1hv1g1m00u52.apps.googleusercontent.com'
 SECRET_KEY = 'zMN8G1Q7f5QFXAf1_7_zRtPg'
-SQLALCHEMY_DATABASE_URI = 'sqlite:///events.db'
+SQLALCHEMY_DATABASE_URI = 'postgresql://catalog:catalog@localhost/events'
 SQLALCHEMY_TRACK_MODIFICATIONS = True
